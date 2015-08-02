8377674e81951554c458672a336d9e94be0548c6
Alec Lomas <alec@lowmess.com>
2015-08-01 19:57:31 -0700

    Value Props
    * Started work on value props background image processing & presentation

--- site/blueprints/value-prop.php
+++ site/blueprints/value-prop.php
@@ -2,8 +2,9 @@
 
 title: Value Prop
 pages: false
-files: false
-preview: parent
+files:
+  type: image
+preview: false
 visible: true
 fields:
   instructions:
@@ -14,9 +15,16 @@ fields:
 
       Please ensure that the page is 'visible'. You can do this by clicking the option in the sidebar. If the page is set to 'invisible', the value prop will not be displayed. On account of it being invisible.
 
+      #### Images
+
+      Please upload a related image for use as the background. The bigger, the better. If you don't have a high resolution image, I suggest using (link: //finda.photo text: FindA.Photo) to find a high-quality (and royalty-free!) stock image.
+
+
+      Please only upload one image. Once in use, it will be renamed to '_background' and resized for responsive purposes. If you wish to replace the image, simply remove the file named '_background' and replace it with one named literally anything else. Please note that any image not named '_background' will be deleted if '_background' already exists.
+
       #### Icons
 
-      Available icons can be found on (link:ionicons.com text: Ionicons). Simply click the icon you which to use, and get the name minus the ```'ion-'``` prefix. e.g., the very first icon on (link:ionicons.com text: Ionicons) would be 'ionic'.
+      Available icons can be found on (link: //ionicons.com text: Ionicons). Simply click the icon you which to use, and get the name minus the ```'ion-'``` prefix. e.g., the very first icon on (link: //ionicons.com text: Ionicons) would be 'ionic'.
   title:
     label: Title
     type: text
--- site/snippets/value-props.php
+++ site/snippets/value-props.php
@@ -3,13 +3,118 @@
     <h2><?php echo $data->headline()->html() ?></h2>
   <?php endif; ?>
   <?php echo $data->text()->kirbytext() ?>
-  <?php foreach($data->children()->visible() as $vp): ?>
+  <?php foreach($data->children()->visible() as $vp):
+
+    // Let's make some new images, maybe? Only if we need to tho. And there are images that exist
+    if($vp->hasImages()) { // && $vp->images()->count() <= 1) {
+
+      $bgExists = false;
+      $resize   = false;
+
+      foreach($vp->images() as $image) { // try to find a background
+        $n = strpos($image->dir(), '_background');
+        if ($n !== false) $bgExists = true;
+        if ($bgExists) break;
+      }
+
+      if(!$bgExists) {
+
+        foreach($vp->images() as $image) { //delete old background images
+          $q = strpos($image->dir(), '_background@');
+          if ($q !== false) {
+            $image->delete();
+          }
+        }
+
+        foreach($vp->images() as $image) { // find new background image
+          $q = strpos($image->dir(), '_background');
+          if ($q === false) {
+            $image->rename('_background');
+            $bgExists = true;
+            $resize = true;
+          }
+          if ($resize) break;
+        }
+      }
+
+      if ($resize) { // make responsive sizes
+        $img = $vp->images()->first();
+        $imgHeight = $img->height();
+        $imgWidth = $img->width();
+
+        $imgURL = $img->url();
+        $imgRoot = $img->dir();
+        $imgPage = $img->page();
+        $imgExt = $img->extension();
+        $imgName = str_replace($img->dir().'\\', '', $vp->images()->first());
+        $imgTitle = str_replace('.'.$imgExt, '', $imgName);
+
+        if($imgHeight > $imgWidth) { // If portrait
+
+          echo 'huh?';
+
+        } else { // If landscape or square
+
+          if($imgWidth > 1024) { // If image is wide af
+
+            copy($img, $imgRoot.'\\'.$imgTitle.'@3x.'.$imgExt);  // Make a @3x copy
+            if($imgWidth > 2100) { // If image is stupid wide
+              $img3x = imagecreatefrom.$imgExt($imgRoot.'\\'.$imgTitle.'@3x.'.$imgExt);
+              $newHeight = (2100 * $imgHeight) / $imgWidth;
+              imagescale($img3x, 2100, $newHeight); // Scale the @3x copy
+            }
+
+          } elseif ($imgWidth > 640) { // If image is only kinda wide
+
+          }
+        }
+      }
+
+      if ($bgexists && !$resize) { // delete not background images
+        foreach($vp->images() as $image) {
+          $n = strpos($image->dir(), '_background');
+          if ($n === false) {
+            $image->delete();
+          }
+        }
+      }
+
+      $img = $vp->images()->first();
+      $imgSan = $img->page().'/'.str_replace($img->dir().'\\', '', $img);
+
+    // Now let's set these suckers (if they exist)
+    if(isset($img)): ?>
+      <style media="screen">
+        .value__background--<?php echo $vp->title()->html() ?> {
+          background-image: url(<?php echo $imgSan ?>);
+        }
+        <?php if(isset($img2x)): ?>
+          @media only screen and (min-width: 640px) {
+            .value__background--<?php echo $vp->title()->html() ?> {
+              background-image: url(<?php echo $imgSan ?>);
+            }
+          }
+        <?php endif; ?>
+        <?php if(isset($img3x)): ?>
+          @media only screen and (min-width: 1024px) {
+            .value__background--<?php echo $vp->title()->html() ?> {
+              background-image: url(<?php echo $imgSan ?>);
+            }
+          }
+        <?php endif; ?>
+      </style>
+    <?php endif; ?>
+
     <section class ="value__prop value__prop--<?php echo $vp->title()->html() ?>">
-      <div class="value__icon">
-        <i class="icon ion-<?php echo $vp->icon()->html() ?>"></i>
+      <div class="value__img">
+        <div class="value__background value__background--<?php echo $vp->title()->html() ?>"></div>
+        <div class="value__overlay"></div>
       </div>
       <div class="value__text">
-        <h3><?php echo $vp->title()->html() ?></h3>
+        <div class="value__headline">
+          <h3><?php echo $vp->title()->html() ?></h3>
+          <i class="value__icon icon ion-<?php echo $vp->icon()->html() ?>"></i>
+        </div>
         <?php echo $vp->text()->kirbytext() ?>
       </div>
     </section>
