5f726abf9b9fc3995e2e9c4f9ff650a3b8c61965
Alec Lomas <alec@lowmess.com>
2016-04-01 04:10:48 -0700

    Switch to JS Standard

--- source/scripts/selectors.js
+++ source/scripts/selectors.js
@@ -1,10 +1,10 @@
 // Returns an individual node (first node that querySelector matches)
 // @link https://gist.github.com/lowmess/9cf8b203c6507ba05e15
 
-function $ ( node ) {
-  return document.querySelector(node);
+function $ (node) {
+  return document.querySelector(node)
 }
 // Returns a node list
-function $$ ( nodeList ) {
-  return document.querySelectorAll(nodeList);
+function $$ (nodeList) {
+  return document.querySelectorAll(nodeList)
 }
