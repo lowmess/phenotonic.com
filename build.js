0071ca97d951aa011f01b30c9523c618b6e292c3
Alec Lomas <alec@lowmess.com>
2016-04-01 04:21:04 -0700

    Remove imagemin
    * metalsmith-imagemin is once again failing
    
    * Standardize build.js

--- build.js
+++ build.js
@@ -1,41 +1,41 @@
-var Metalsmith    = require('metalsmith');
-var sitemap       = require('metalsmith-mapsite');
-var feed          = require('metalsmith-feed');
-var defaultValues = require('metalsmith-default-values');
+var Metalsmith = require('metalsmith')
+var sitemap = require('metalsmith-mapsite')
+var feed = require('metalsmith-feed')
+var defaultValues = require('metalsmith-default-values')
 // HTML
-var layouts       = require('metalsmith-layouts');
-var drafts        = require('metalsmith-drafts');
-var snippet       = require('metalsmith-snippet');
-var markdown      = require('metalsmith-markdown');
-var permalinks    = require('metalsmith-permalinks');
-var collections   = require('metalsmith-collections');
-var pagination    = require('metalsmith-pagination');
-var tags          = require('metalsmith-tags');
+var layouts = require('metalsmith-layouts')
+var drafts = require('metalsmith-drafts')
+var snippet = require('metalsmith-snippet')
+var markdown = require('metalsmith-markdown')
+var permalinks = require('metalsmith-permalinks')
+var collections = require('metalsmith-collections')
+var pagination = require('metalsmith-pagination')
+var tags = require('metalsmith-tags')
 // CSS
-var sass          = require('metalsmith-sass');
-var prefix        = require('metalsmith-autoprefixer');
+var sass = require('metalsmith-sass')
+var prefix = require('metalsmith-autoprefixer')
 // JS
-var uglify        = require('metalsmith-uglify');
+var uglify = require('metalsmith-uglify')
 // IMAGES
-var imagemin      = require('metalsmith-imagemin');
+// var imagemin = require('metalsmith-imagemin')
 
 Metalsmith(__dirname)
   .source('source')
   .destination('_build')
   .metadata({
     site: {
-      url: "https://www.phenotonic.com/",
-      title: "Phenotonic",
-      description: "Phenotonic provides the tools, expertise, and education for gardens of all varieties & gardeners of all skill levels.",
-      keywords: "grow, growing, garden, gardening, consulting, consultants, consultant, consult, experts, expert, expertise, mmj, marijuana, vegetable, hydroponic, organic, hydroponics, hydro",
+      url: 'https://www.phenotonic.com/',
+      title: 'Phenotonic',
+      description: 'Phenotonic provides the tools, expertise, and education for gardens of all varieties & gardeners of all skill levels.',
+      keywords: 'grow, growing, garden, gardening, consulting, consultants, consultant, consult, experts, expert, expertise, mmj, marijuana, vegetable, hydroponic, organic, hydroponics, hydro',
       snipcart: {
-        key: "NmNhYWY2MGYtZTljYi00YzE4LThlNjktNGRhMGE2OTM2ZjAx"
+        key: 'NmNhYWY2MGYtZTljYi00YzE4LThlNjktNGRhMGE2OTM2ZjAx'
       }
     },
     landing: {
-      headline: "Introducing the Bloom Box",
-      copy: "We made the best organic CO2 generator in the universe to give plants the happy life they deserve.",
-      url: "/store/bloom-box",
+      headline: 'Introducing the Bloom Box',
+      copy: 'We made the best organic CO2 generator in the universe to give plants the happy life they deserve.',
+      url: '/store/bloom-box',
       button: "Let's Grow"
     }
   })
@@ -50,9 +50,9 @@ Metalsmith(__dirname)
   .use(uglify({
     concat: 'js/main.js',
     removeOriginal: true
-    }))
+  }))
   // IMAGES
-  .use(imagemin())
+  // .use(imagemin())
   // HTML
   .use(drafts())
   .use(collections({
@@ -110,13 +110,13 @@ Metalsmith(__dirname)
     relative: false,
     linksets: [{
       match: { collection: 'pages' },
-      pattern: ':title',
+      pattern: ':title'
     }]
   }))
   .use(tags({
     handle: 'tags',
-    path:'tagged/:tag/index.html',
-    pathPage: "tagged/:tag/:num/index.html",
+    path: 'tagged/:tag/index.html',
+    pathPage: 'tagged/:tag/:num/index.html',
     perPage: 10,
     layout: 'tag.jade',
     sortBy: 'date',
@@ -138,6 +138,6 @@ Metalsmith(__dirname)
   }))
   .use(sitemap('https://phenotonic.com'))
   .use(feed({collection: 'blog'}))
-  .build(function(err) {
-    if (err) throw err;
-  });
+  .build(function (err) {
+    if (err) throw err
+  })
